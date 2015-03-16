a0b8ef32372cef38c4f7bf7b9dc9261f4cdc3525
Frederick Hewett <fred.hewett@gmail.com>
2015-03-16 12:33:07 -0400

    Initial Commit

---
+++ buildjson/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  buildjson
+//
+//  Created by Fred Hewett on 3/16/15.
+//  Copyright (c) 2015 Frederick Hewett. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
